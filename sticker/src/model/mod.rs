stickeritis/sticker2

commit ddbb072032c2a819dff1f5672487c255516eea98

Add support for on-demand position embeddings
This change adds the `position_embeddings` option to the `model`
section of the configuration. This option currently has one of two
possible values:

- `model`: use the position embeddings in the model parameter file.
- `sinusoidal`: use generated sinusoidal embeddings.

sticker/src/model/mod.rs
@@ -9,18 +9,54 @@ use sticker_transformers::layers::Dropout;
 use sticker_transformers::models::bert::{
     BertConfig, BertEmbeddings, BertEncoder, BertError, BertLayerOutput,
 };
+use sticker_transformers::models::sinusoidal::SinusoidalEmbeddings;
 use sticker_transformers::scalar_weighting::{
     ScalarWeightClassifier, ScalarWeightClassifierConfig,
 };
 use tch::nn::{ModuleT, Path};
 use tch::{self, Kind, Tensor};
 
+use crate::config::PositionEmbeddings;
 use crate::encoders::Encoders;
+
+#[derive(Debug)]
+enum BertEmbeddingLayer {
+    Bert(BertEmbeddings),
+    Sinusoidal(SinusoidalEmbeddings),
+}
+
+impl BertEmbeddingLayer {
+    fn new<'a>(
+        vs: impl Borrow<Path<'a>>,
+        config: &BertConfig,
+        position_embeddings: PositionEmbeddings,
+    ) -> Self {
+        match position_embeddings {
+            PositionEmbeddings::Model => {
+                BertEmbeddingLayer::Bert(BertEmbeddings::new(vs, config, true))
+            }
+            PositionEmbeddings::Sinusoidal => {
+                BertEmbeddingLayer::Sinusoidal(SinusoidalEmbeddings::new(vs, config))
+            }
+        }
+    }
+}
+
+impl ModuleT for BertEmbeddingLayer {
+    fn forward_t(&self, input: &Tensor, train: bool) -> Tensor {
+        use BertEmbeddingLayer::*;
+
+        match self {
+            Bert(ref embeddings) => embeddings.forward_t(input, train),
+            Sinusoidal(ref embeddings) => embeddings.forward_t(input, train),
+        }
+    }
+}
 
 /// Multi-task classifier using the BERT architecture with scalar weighting.
 #[derive(Debug)]
 pub struct BertModel {
-    embeddings: BertEmbeddings,
+    embeddings: BertEmbeddingLayer,
     encoder: BertEncoder,
     classifiers: HashMap<String, ScalarWeightClassifier>,
     layers_dropout: Dropout,
@@ -36,10 +72,11 @@ impl BertModel {
         config: &BertConfig,
         encoders: &Encoders,
         layers_dropout: f64,
+        position_embeddings: PositionEmbeddings,
     ) -> Result<Self, BertError> {
         let vs = vs.borrow();
 
-        let embeddings = BertEmbeddings::new(vs.sub("encoder"), config, true);
+        let embeddings = BertEmbeddingLayer::new(vs, config, position_embeddings);
         let encoder = BertEncoder::new(vs.sub("encoder"), config)?;
 
         let classifiers = encoders
@@ -87,11 +124,11 @@ impl BertModel {
 
         let pretrained_file = File::open(hdf_path, "r")?;
 
-        let embeddings = BertEmbeddings::load_from_hdf5(
+        let embeddings = BertEmbeddingLayer::Bert(BertEmbeddings::load_from_hdf5(
             vs.sub("encoder"),
             config,
             pretrained_file.group("bert/embeddings")?,
-        )?;
+        )?);
 
         let encoder = BertEncoder::load_from_hdf5(
             vs.sub("encoder"),
@@ -139,9 +176,9 @@ impl BertModel {
         freeze_encoder: bool,
     ) -> Vec<BertLayerOutput> {
         let embeds = if freeze_embeddings {
-            tch::no_grad(|| self.embeddings.forward_t(inputs, None, None, train))
+            tch::no_grad(|| self.embeddings.forward_t(inputs, train))
         } else {
-            self.embeddings.forward_t(inputs, None, None, train)
+            self.embeddings.forward_t(inputs, train)
         };
 
         let mut encoded = if freeze_encoder {

shell.nix
@@ -21,5 +21,5 @@ in with nixpkgs; mkShell {
   # it if libraries and includes are in different directories.
   HDF5_DIR = symlinkJoin { name = "hdf5-join"; paths = [ hdf5.dev hdf5.out ]; };
 
-  LIBTORCH = "${danieldk.libtorch.v1_3_1}";
+  LIBTORCH = "${danieldk.libtorch.v1_4_0}";
 }

sticker-utils/Cargo.toml
@@ -18,6 +18,6 @@ serde_yaml = "0.8"
 stdinout = "0.4"
 sticker = { path = "../sticker" }
 sticker-encoders = "0.1.4"
-sticker-transformers = "0.2"
-tch = "0.1.3"
+sticker-transformers = "0.3"
+tch = "0.1.5"
 threadpool = "1"

sticker-utils/src/io.rs
@@ -50,8 +50,14 @@ impl Model {
 
         let mut vs = VarStore::new(device);
 
-        let model = BertModel::new(vs.root(), &bert_config, &encoders, 0.0)
-            .or_exit("Cannot construct model", 1);
+        let model = BertModel::new(
+            vs.root(),
+            &bert_config,
+            &encoders,
+            0.0,
+            config.model.position_embeddings,
+        )
+        .or_exit("Cannot construct model", 1);
 
         vs.load(parameters_path)
             .or_exit("Cannot load model parameters", 1);

sticker/Cargo.toml
@@ -23,8 +23,8 @@ rand_xorshift = "0.2"
 serde = { version = "1", features = [ "derive" ] }
 serde_json = "1"
 sticker-encoders = "0.1.4"
-sticker-transformers = "0.2"
-tch = "0.1.3"
+sticker-transformers = "0.3"
+tch = "0.1.5"
 toml = "0.5"
 wordpieces = "0.1.1"
 

sticker/src/config.rs
@@ -55,6 +55,9 @@ pub struct Model {
     /// Model parameters.
     pub parameters: String,
 
+    /// Configuration of position embeddings.
+    pub position_embeddings: PositionEmbeddings,
+
     /// Configuration of the pretrained model.
     pub pretrain_config: String,
 }
@@ -65,6 +68,16 @@ impl Model {
         let f = File::open(&self.pretrain_config)?;
         Ok(serde_json::from_reader(BufReader::new(f))?)
     }
+}
+
+#[derive(Clone, Debug, Deserialize, Eq, PartialEq, Serialize)]
+#[serde(rename_all = "snake_case")]
+pub enum PositionEmbeddings {
+    /// Use position embeddings from the model.
+    Model,
+
+    /// Use generated sinusoidal embeddings.
+    Sinusoidal,
 }
 
 /// Sequence labeler configuration.
@@ -153,7 +166,7 @@ mod tests {
     use sticker_encoders::layer::Layer;
     use sticker_encoders::lemma::BackoffStrategy;
 
-    use crate::config::{Config, Input, Labeler, Model, TomlRead};
+    use crate::config::{Config, Input, Labeler, Model, PositionEmbeddings, TomlRead};
     use crate::encoders::{DependencyEncoder, EncoderType, EncodersConfig, NamedEncoderConfig};
 
     #[test]
@@ -186,6 +199,7 @@ mod tests {
                 },
                 model: Model {
                     parameters: "epoch-99".to_string(),
+                    position_embeddings: PositionEmbeddings::Model,
                     pretrain_config: "bert_config.json".to_string(),
                 }
             }

sticker/testdata/sticker.conf
@@ -11,4 +11,5 @@ encoders = [
 
 [model]
 parameters = "epoch-99"
+position_embeddings = "model"
 pretrain_config = "bert_config.json"

commit 20ef303ce753b7946dcec52c14e51fb1e8c656dd

Fixup incorrect embedding path introduced in ddbb072032

sticker/src/model/mod.rs
@@ -76,7 +76,7 @@ impl BertModel {
     ) -> Result<Self, BertError> {
         let vs = vs.borrow();
 
-        let embeddings = BertEmbeddingLayer::new(vs, config, position_embeddings);
+        let embeddings = BertEmbeddingLayer::new(vs.sub("encoder"), config, position_embeddings);
         let encoder = BertEncoder::new(vs.sub("encoder"), config)?;
 
         let classifiers = encoders