a59acedb9d4ae13a6337747de9c59cb05f1d1a3f
Daniël de Kok <me@danieldk.eu>
2020-03-25 14:01:07 +0100

    Add a progress bar to the prepare subcommand

--- sticker2-utils/src/subcommands/prepare.rs
+++ sticker2-utils/src/subcommands/prepare.rs
@@ -1,15 +1,17 @@
 use std::fs::File;
-use std::io::Write;
+use std::io::{BufReader, Write};
 
 use clap::{App, Arg, ArgMatches};
 use conllu::io::{ReadSentence, Reader};
+use indicatif::ProgressStyle;
 use serde_yaml;
-use stdinout::{Input, OrExit};
+use stdinout::OrExit;
 use sticker2::config::Config;
 use sticker2::encoders::Encoders;
 use sticker_encoders::SentenceEncoder;
 
 use crate::io::load_config;
+use crate::progress::ReadProgress;
 use crate::traits::{StickerApp, DEFAULT_CLAP_SETTINGS};
 
 const CONFIG: &str = "CONFIG";
@@ -17,7 +19,7 @@ static TRAIN_DATA: &str = "TRAIN_DATA";
 
 pub struct PrepareApp {
     config: String,
-    train_data: Option<String>,
+    train_data: String,
 }
 
 impl PrepareApp {
@@ -41,12 +43,17 @@ impl StickerApp for PrepareApp {
                     .index(1)
                     .required(true),
             )
-            .arg(Arg::with_name(TRAIN_DATA).help("Training data").index(2))
+            .arg(
+                Arg::with_name(TRAIN_DATA)
+                    .help("Training data")
+                    .index(2)
+                    .required(true),
+            )
     }
 
     fn parse(matches: &ArgMatches) -> Self {
         let config = matches.value_of(CONFIG).unwrap().into();
-        let train_data = matches.value_of(TRAIN_DATA).map(ToOwned::to_owned);
+        let train_data = matches.value_of(TRAIN_DATA).unwrap().into();
 
         PrepareApp { config, train_data }
     }
@@ -56,12 +63,14 @@ impl StickerApp for PrepareApp {
 
         let encoders: Encoders = (&config.labeler.encoders).into();
 
-        let input = Input::from(self.train_data.as_ref());
-        let treebank_reader = Reader::new(
-            input
-                .buf_read()
-                .or_exit("Cannot open corpus for reading", 1),
-        );
+        let train_file = File::open(&self.train_data).or_exit("Cannot open train data file", 1);
+        let read_progress = ReadProgress::new(train_file).or_exit("Cannot create progress bar", 1);
+        let progress_bar = read_progress.progress_bar().clone();
+        progress_bar.set_style(ProgressStyle::default_bar().template(&format!(
+            "[Time: {{elapsed_precise}}, ETA: {{eta_precise}}] {{bar}} {{percent}}% {{msg}}",
+        )));
+
+        let treebank_reader = Reader::new(BufReader::new(read_progress));
 
         for sentence in treebank_reader.sentences() {
             let sentence = sentence.or_exit("Cannot read sentence from treebank", 1);
